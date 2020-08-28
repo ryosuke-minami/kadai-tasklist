5157c6331543a189bcd8e2b85f8850bc0ceefda0
ryosuke <e5a2w942cm@outlook.jp>
2020-08-28 13:38:34 +0000

    user regisration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,8 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welocome to the TaskList</h1>
+            {{--ユーザ登録ページへのリンク--}}
+            {!! link_to_route('signup.get','Sign up now!',[],['class'=>'btn btn-primary btn-block']) !!}
         </div>
     </div>
 @endsection
