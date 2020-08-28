41356c91eeadcf3ad4a2a5d35a9cbab5dfd9c0e8
ryosuke <e5a2w942cm@outlook.jp>
2020-08-28 14:20:50 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron">
-        <div class="text-center">
-            <h1>Welocome to the TaskList</h1>
-            {{--ユーザ登録ページへのリンク--}}
-            {!! link_to_route('signup.get','Sign up now!',[],['class'=>'btn btn-primary btn-block']) !!}
+    @if(Auth::check())
+        {{Auth::user->name}}
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welocome to the TaskList</h1>
+                {{--ユーザ登録ページへのリンク--}}
+                {!! link_to_route('signup.get','Sign up now!',[],['class'=>'btn btn-primary btn-lg']) !!}
+            </div>
         </div>
-    </div>
+    @endif
 @endsection
