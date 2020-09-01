d7479028ac61d99084b4e522cf872dd45351e84e
ryosuke <e5a2w942cm@outlook.jp>
2020-09-01 11:52:22 +0000

    finish

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -16,7 +16,7 @@ class TasksController extends Controller
     public function index()
     {
         if(\Auth::check()){
-            $tasks=Task::all();
+            $tasks=Task::where('user_id',\Auth::user()->id)->get();
             return view('tasks.index',['tasks'=>$tasks,]);            
         }
         else{
@@ -45,7 +45,7 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         $user = \Auth::user();
-        $request->validate(['status'=>'required|max:10',]);
+        $request->validate(['status'=>'required|max:10','content'=>'required|max:255',]);
         $task=new Task;
         $task->status=$request->status;
         $task->content=$request->content;
@@ -93,7 +93,7 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $request->validate(['status'=>'required|max:10',]);
+        $request->validate(['status'=>'required|max:10','content'=>'required|max:255',]);
         $task=Task::findOrFail($id);
         $task->status=$request->status;
         $task->content=$request->content;
